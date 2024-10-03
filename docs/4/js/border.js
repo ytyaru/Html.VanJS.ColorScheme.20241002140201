f693b71d0101e7ace62cee74944c5a202d482e39
ytyaru <yttry0@gmail.com>
2024-10-03 16:27:49 +0900

    add 4

---
+++ docs/4/js/border.js
@@ -0,0 +1,24 @@
+;(function(){
+// table, input, 
+// border-[width,style,color]
+class BorderStyle {
+    constructor(options) {
+        this._els = {
+            table: {},
+            tr: {}
+            th: {},
+            td: {},
+            tbody: {},
+            thead: {},
+            caption: {},
+            input: {},
+        }
+    }
+}
+// text-decoration-[style,color,thickness], text-underline-offset
+class TextDecoration {
+
+}
+
+})();
+
